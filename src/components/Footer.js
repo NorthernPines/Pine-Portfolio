e9aca90d04a05e39c8bb3c2d80efdaf232d2911b
Northern Pines <traviswalborn8@gmail.com>
2022-09-21 23:03:08 -0400

    footer touch up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import img1 from "./../images/GitHub_Logo.png";
-import img2 from "./../images/LI-In-Bug.png"
+import img2 from "./../images/LI-In-Bug.png";
+import img3 from "./../images/gitlab-logo-100.jpg";
 
 export default function Footer() {
     return (
         <div className='container p-2'>
             <div className='d-flex justify-content-center'>
-                <a className='p-4' href="https://github.com/NorthernPines">
+                <a className='p-5' href="https://github.com/NorthernPines">
                     <img src={img1} alt="GitHub" width="180"/>
                 </a>
-                <a className='p-4' href="https://www.linkedin.com/in/travis-walborn-5136b5243/">
+                <a className='p-5' href="https://www.linkedin.com/in/travis-walborn-5136b5243/">
                     <img src={img2} alt="Linked In" width="72"/>
+                </a>
+                <a className='p-3' href="https://upenn.bootcampcontent.com/traviswalborn8-OaXWMU">
+                    <img src={img3} alt="GitLab" width="300"/>
                 </a>
             </div>
         </div>
